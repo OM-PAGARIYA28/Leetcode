f879306c0768a197df5008dfd9c5abddfbad8c47
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-03-03 22:15:04 +0530

    Time: 8 ms (40.79%) | Memory: 42 MB (81.64%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -19,3 +19,28 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+
+// class Solution {
+//     public String removeOuterParentheses(String s) {
+//         int count = 0;
+//         StringBuilder str = new StringBuilder();
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == '(') {
+//                 if (count != 0) {
+//                     str.append(s.charAt(i));
+//                 }
+//                 count++;
+//             } else {
+//                 count--;
+//                 if (count != 0) {
+//                     str.append(s.charAt(i));
+//                 }
+//             }
+//         } 
+
+//         return str.toString();
+//     }
+// }
