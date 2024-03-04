55dee4799420f942eab6d4e72a6f4efec0aa109d
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-03-04 15:38:57 +0530

    Time: 3 ms (37.26%) | Memory: 42.5 MB (85.03%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.java
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -3,25 +3,23 @@ class Solution {
         Arrays.sort(tokens);
         int count=0;
         int left=0;
-        int max=0;
         int right=tokens.length-1;
         while(left<=right){
             if(tokens[left]<=power){
                 power-=tokens[left];
                 count++;
                 left++;
-                max = Math.max(max, count);
             }
-            else if(count>=1 ){
+            else if(count>=1 && tokens[left]>power ){
+                if(left==right){break;}
                 power+=tokens[right];
                 count--;
-                right--;
-                 
+                right--;   
             }
             else{
                 break;
             }
         }
-        return max;
+        return count;
     }
 }
