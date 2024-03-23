1e9614c782dd80c38673b73ae3943d5671965ba9
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-03-23 13:15:27 +0530

    Time: 2 ms (88.69%) | Memory: 47.8 MB (87.26%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,42 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
+        if(head == null){
+            return ;
         }
-        
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode slow=head;
+        ListNode fast =head;
+        while(fast!=null && fast.next!=null ){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        
-        ListNode reversed = reverse(slow.next);
-        slow.next = null; 
-        
-        merge(head, reversed);
-    }
-    
-    private ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        while (current != null) {
-            ListNode next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+         ListNode reverse= reverse(slow.next);
+         slow.next=null;
+
+        while(reverse!=null){
+            ListNode front=head.next;
+            ListNode back=reverse.next;
+            head.next = reverse; 
+            reverse.next=front;
+            head = front;
+            reverse=back;
         }
-        return prev;
-    }
-    
-    private void merge(ListNode first, ListNode second) {
-        while (second != null) {
-            ListNode temp1 = first.next;
-            ListNode temp2 = second.next;
-            first.next = second;
-            second.next = temp1;
-            first = temp1;
-            second = temp2;
+        }
+
+
+    public ListNode reverse(ListNode head){
+            ListNode prev=null;
+            ListNode current=head;
+            ListNode next;
+            while(current!=null){
+                next=current.next;
+                current.next=prev;
+                prev=current;
+                current=next;
+            }
+            return prev;
         }
     }
-}
