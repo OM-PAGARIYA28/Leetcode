67b4efadcc38b786f847fe65bad0c2506551b809
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-03-23 14:10:24 +0530

    Time: 15 ms (19.81%) | Memory: 49.3 MB (76.00%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -4,24 +4,42 @@ class Solution {
         // int n = nums.length;
         // return nums[n/2];
 
-        int a=nums[0];
-        int count=1;
-        for(int i=1;i<nums.length;i++){
-            if(a==nums[i]){
-                count++;
-            }
-            else{
-                if(count==0){
-                   a=nums[i];
-                   count++;
-                }
-                else{
-                    count--;
-                }
+//         int a=nums[0];
+//         int count=1;
+//         for(int i=1;i<nums.length;i++){
+//             if(a==nums[i]){
+//                 count++;
+//             }
+//             else{
+//                 if(count==0){
+//                    a=nums[i];
+//                    count++;
+//                 }
+//                 else{
+//                     count--;
+//                 }
                 
-            }
+//             }
 
-        }
-        return a;
+//         }
+//         return a;
+//     }
+// }
+
+HashMap<Integer,Integer> map=new HashMap<>();
+int n=nums.length;
+for(int i=0;i<n;i++){
+    if(map.containsKey(nums[i])){
+        map.put(nums[i],map.get(nums[i])+1);
+    }
+    else{
+        map.put(nums[i],1);
+    }
+}
+for(int key:map.keySet() ){
+    if(map.get(key)>(n/2))
+    return key;
+}
+return -1;
     }
 }
