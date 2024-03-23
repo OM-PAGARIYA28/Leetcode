7ad6ab31519cd81b4d258fd9812d404d799f0585
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-03-24 01:40:02 +0530

    Time: 2 ms (93.30%) | Memory: 43.8 MB (7.95%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,27 +1,47 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
 
-        int[] result = new int[Math.min(nums1.length, nums2.length)];
-        int index = 0;
-        int i = 0;
-        int j = 0;
+        // int[] result = new int[Math.min(nums1.length, nums2.length)];
+        // int index = 0;
+        // int i = 0;
+        // int j = 0;
 
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] == nums2[j]) {
-                if (index == 0 || nums1[i] != result[index - 1]) {
-                    result[index++] = nums1[i];
-                }
-                i++;
-                j++;
-            } else if (nums1[i] < nums2[j]) {
-                i++;
-            } else {
-                j++;
+        // while (i < nums1.length && j < nums2.length) {
+        //     if (nums1[i] == nums2[j]) {
+        //         if (index == 0 || nums1[i] != result[index - 1]) {
+        //             result[index++] = nums1[i];
+        //         }
+        //         i++;
+        //         j++;
+        //     } else if (nums1[i] < nums2[j]) {
+        //         i++;
+        //     } else {
+        //         j++;
+        //     }
+        // }
+
+        // return Arrays.copyOf(result, index);
+
+        // int count=0;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums1.length;i++){
+            set.add(nums1[i]);
+        }
+        HashSet<Integer> intersectionSet = new HashSet<>();
+        for (int j = 0; j < nums2.length; j++) {
+            if (set.contains(nums2[j])) {
+                intersectionSet.add(nums2[j]);
             }
         }
 
-        return Arrays.copyOf(result, index);
+        // Convert intersection set to array
+        int[] result = new int[intersectionSet.size()];
+        int index = 0;
+        for (int num : intersectionSet) {
+            result[index++] = num;
+        }
+        return result;
     }
 }
