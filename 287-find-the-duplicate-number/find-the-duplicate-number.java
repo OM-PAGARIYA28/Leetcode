5f6b29a37563d2a2a9b6ad0ccb25a681c2030dc8
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-03-24 11:41:26 +0530

    Time: 20 ms (36.00%) | Memory: 56.4 MB (93.70%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,19 +1,27 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-       HashMap<Integer,Integer> map=new HashMap<>();
-       for(int i=0;i<nums.length;i++){
-        if(map.containsKey(nums[i])){
-            map.put(nums[i],map.get(nums[i])+1);
-        }
-        else{
-            map.put(nums[i],1);
-        }
-       }
-       for(int i=0;i<nums.length;i++){
-        if(map.get(nums[i])>1){
+    //    HashMap<Integer,Integer> map=new HashMap<>();
+    //    for(int i=0;i<nums.length;i++){
+    //     if(map.containsKey(nums[i])){
+    //         map.put(nums[i],map.get(nums[i])+1);
+    //     }
+    //     else{
+    //         map.put(nums[i],1);
+    //     }
+    //    }
+    //    for(int i=0;i<nums.length;i++){
+    //     if(map.get(nums[i])>1){
+    //         return nums[i];
+    //     }
+    //    } 
+
+      HashSet<Integer> h=new HashSet<>();
+      for(int i=0;i<nums.length;i++){
+        if(h.contains(nums[i])){
             return nums[i];
         }
-       } 
+        h.add(nums[i]);
+      }
        return 0;
     }
 }
