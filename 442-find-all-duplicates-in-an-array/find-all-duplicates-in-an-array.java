43b8ff48205abde5fb6b9c263f45d261f46de28c
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-03-25 15:13:35 +0530

    Time: 17 ms (34.49%) | Memory: 55 MB (28.18%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,20 +1,36 @@
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         List<Integer> duplicate=new ArrayList<>();
+//         for(int i=0;i<nums.length;i++){
+//             if(map.containsKey(nums[i])){
+//                 map.put(nums[i],map.get(nums[i])+1);
+//             }
+//             else{
+//                 map.put(nums[i],1);
+//             }
+//         }
+//         for(int num : map.keySet()){
+//             if(map.get(num)>1){
+//                 duplicate.add(num);
+//             }
+//         }
+//         return duplicate;
+//     }
+// }
+
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        List<Integer> duplicate=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }
-            else{
-                map.put(nums[i],1);
-            }
-        }
-        for(int num : map.keySet()){
-            if(map.get(num)>1){
-                duplicate.add(num);
+        var dup=new ArrayList<Integer>();
+        var data=new HashSet<Integer>();
+        for(int num:nums){
+            if(data.contains(num)){
+                dup.add(num);
+            }else{
+                data.add(num);
             }
         }
-        return duplicate;
+        return dup;
+        
     }
 }
