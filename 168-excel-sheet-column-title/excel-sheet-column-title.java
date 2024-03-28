c3bdabcdc984fc97edcfa93be688ccbf0f5030ac
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-03-29 00:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (82.15%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -9,4 +9,20 @@ public class Solution {
         }
         return result.toString();
     }
+    //  StringBuilder columnName = new StringBuilder();
+        
+    //     while (columnNumber > 0) {
+    //         int rem = columnNumber % 26;
+
+    //         if (rem == 0) {
+    //             columnName.append("Z");
+    //             columnNumber = (columnNumber / 26) - 1;
+    //         } else {
+    //             columnName.append((char)((rem - 1) + 'A'));
+    //             columnNumber = columnNumber / 26;
+    //         }
+    //     }
+
+    //     return columnName.reverse().toString();
+    // }
 }
