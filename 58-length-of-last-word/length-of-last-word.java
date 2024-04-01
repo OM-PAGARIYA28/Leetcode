6b9e4b7e66b54a82e902892871ceb6e1ae6bb3df
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-04-01 09:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (30.67%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,13 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+        s=s.trim();
         int count = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == ' ') {
-                if (count > 0) {
-                    break;
-                }
+            if (s.charAt(i) != ' ') {
+                // if (count > 0) {
+                    count++;
+                // }
             } else {
-                count++;
+                break;
             }
         }
         return count;
