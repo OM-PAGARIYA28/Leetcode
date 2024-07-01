1aaaebaf95fcfd3730a9905636e90d3f1c03feff
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-07-01 23:47:03 +0530

    Time: 1824 ms (5.12%) | Memory: 47.5 MB (60.75%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,29 +1,54 @@
+// class Solution {
+//     public int numSubarraysWithSum(int[] nums, int goal) {
+//         int i = 0;
+//         int j = 0;
+//         int sum = 0;
+//         int count = 0;
+//         int freq = 0;
+
+//         while(i < nums.length) {
+//             sum += nums[i];
+//             if(nums[i] == 1) {
+//                 freq = 0;
+//             }
+//             if(sum > goal) {
+//                 sum -= nums[j];
+//                 j++;
+//             }
+            
+//             while(j <= i && sum == goal) {
+//                 sum -= nums[j];
+//                 j++;
+//                 freq++;
+//             }
+//             count += freq;
+//             i++;
+//         }
+//         return count;
+//     }
+// }
+
+
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int i = 0;
-        int j = 0;
-        int sum = 0;
+        int sum =0 ;
         int count = 0;
-        int freq = 0;
+        int n = nums.length;
 
-        while(i < nums.length) {
-            sum += nums[i];
-            if(nums[i] == 1) {
-                freq = 0;
+
+        for(int i =0;i<n;i++)
+        {
+            sum =0 ;
+            for(int j =i;j<n;j++)
+            {
+                sum +=nums[j];
+                if(sum==goal)
+                {
+                    count++;
+                }
             }
-            if(sum > goal) {
-                sum -= nums[j];
-                j++;
-            }
-            
-            while(j <= i && sum == goal) {
-                sum -= nums[j];
-                j++;
-                freq++;
-            }
-            count += freq;
-            i++;
         }
         return count;
+
     }
 }
