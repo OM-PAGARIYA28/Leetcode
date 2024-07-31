325f0386b82698e13fbb24bd0431f559d948b551
OM-PAGARIYA28 <120658989+OM-PAGARIYA28@users.noreply.github.com>
2024-07-31 12:32:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (63.74%) - LeetSync

---
+++ 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumNumbers(int num, int k) {
+        if(num==0){
+            return 0;
+        }
+        if(k==0){
+            if(num%10==0) return 1;
+            else return -1;
+        }
+        for(int i=1;i<=num/k;i++){
+            if(num%10==(k*i)%10) return i;
+        }
+        return -1;
+    }
+}
